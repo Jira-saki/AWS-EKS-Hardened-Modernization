a7dbcaab432d63b4bb6d0c288aafe02782b9bf99
Jira-saki <geepep@gmail.com>
2026-09-03 09:38:14 +0900

    feat(kvm): finalize phase 6 observability, spike testing, and gitops manifests

--- terraform/environments/local-hob/main.tf
+++ terraform/environments/local-hob/main.tf
@@ -1,24 +1,132 @@
-# --- terraform/environments/local-hob/main.tf ---
-
-module "network" {
-  source = "../../modules/network"
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.6"
+    }
+  }
 }
 
-module "compute" {
-  source          = "../../modules/compute"
-  pool_name       = libvirt_pool.ep2_pool.name
-  dmz_net_id      = module.network.dmz_net_id
-  isolated_net_id = module.network.isolated_net_id
+provider "libvirt" {
+  uri = "qemu:///system"
 }
 
+# 1. Storage Pool
 resource "libvirt_pool" "ep2_pool" {
   name = "ep2_pool"
   type = "dir"
   path = "/var/lib/libvirt/images/ep2-pool"
 }
 
+# 2. Base Image Volume
+resource "libvirt_volume" "ubuntu_base" {
+  name   = "ubuntu-jammy-base.qcow2"
+  pool   = libvirt_pool.ep2_pool.name
+  source = "/var/lib/libvirt/images/iso/jammy-server-cloudimg-amd64.img"
+  format = "qcow2"
+}
 
+# 3. Isolated Network Subnet (192.168.150.0/24)
+resource "libvirt_network" "k8s_net" {
+  name      = "ep2-k8s-net"
+  mode      = "nat"
+  domain    = "k8s.local"
+  addresses = ["192.168.150.0/24"]
 
+  dhcp {
+    enabled = true
+  }
 
+  dns {
+    enabled = true
+    hosts {
+      hostname = "controlplane"
+      ip       = "192.168.150.10"
+    }
+    hosts {
+      hostname = "worker-node01"
+      ip       = "192.168.150.11"
+    }
+    hosts {
+      hostname = "worker-node02"
+      ip       = "192.168.150.12"
+    }
+  }
+}
 
+# 4. Node Topology Definitions
+locals {
+  nodes = {
+    "controlplane" = {
+      vcpu   = 2
+      memory = 4096
+      disk   = 32212254720 # 30 GB
+      mac    = "52:54:00:15:00:10"
+      ip     = "192.168.150.10"
+    }
+    "worker-node01" = {
+      vcpu   = 4
+      memory = 16384
+      disk   = 42949672960 # 40 GB
+      mac    = "52:54:00:15:00:11"
+      ip     = "192.168.150.11"
+    }
+    "worker-node02" = {
+      vcpu   = 4
+      memory = 16384
+      disk   = 42949672960 # 40 GB
+      mac    = "52:54:00:15:00:12"
+      ip     = "192.168.150.12"
+    }
+  }
+}
 
+# 5. OS Disks (Differencing Disks on Base Image)
+resource "libvirt_volume" "node_disks" {
+  for_each       = local.nodes
+  name           = "${each.key}-disk.qcow2"
+  pool           = libvirt_pool.ep2_pool.name
+  base_volume_id = libvirt_volume.ubuntu_base.id
+  size           = each.value.disk
+}
+
+# 6. Cloud-Init ISO per Node
+resource "libvirt_cloudinit_disk" "node_init" {
+  for_each = local.nodes
+  name     = "${each.key}-init.iso"
+  pool     = libvirt_pool.ep2_pool.name
+  user_data = templatefile("${path.module}/../../../cloud-init/common-k8s.cfg", {
+    ssh_key = file("~/.ssh/id_ed25519.pub")
+  })
+}
+
+# 7. Virtual Machine Instances
+resource "libvirt_domain" "k8s_nodes" {
+  for_each = local.nodes
+  name     = each.key
+  memory   = each.value.memory
+  vcpu     = each.value.vcpu
+
+  cloudinit = libvirt_cloudinit_disk.node_init[each.key].id
+
+  network_interface {
+    network_id     = libvirt_network.k8s_net.id
+    mac            = each.value.mac
+    wait_for_lease = true
+  }
+
+  disk {
+    volume_id = libvirt_volume.node_disks[each.key].id
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  graphics {
+    type        = "vnc"
+    listen_type = "address"
+  }
+}
--- tests/spike-test.js
+++ tests/spike-test.js
@@ -3,20 +3,21 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },   // Warm-up
-    { duration: '1m',  target: 200 },  // Spike กระตุ้น HPA (2 -> 8+ pods)
-    { duration: '30s', target: 0 },    // Cool-down ดู Scale-in
+    { duration: '30s', target: 20 },  // Ramp-up 20 VUs
+    { duration: '1m',  target: 60 },  // Spike 60 VUs เพื่อดัน CPU ให้เกิน 50%
+    { duration: '30s', target: 0 },   // Scale down
   ],
   thresholds: {
-    http_req_duration: ['p(95)<300'],  // SLA < 300ms
-    http_req_failed: ['rate<0.01'],    // Error rate < 1%
+    http_req_failed: ['rate<0.05'],    // ยอมรับ error rate ไม่เกิน 5%
+    http_req_duration: ['p(95)<1000'], // 95% ของ request ตอบกลับภายใน 1s
   },
 };
 
-const TARGET_URL = __ENV.TARGET_URL || 'http://localhost:30080/healthz';
-
 export default function () {
-  const res = http.get(TARGET_URL);
-  check(res, { 'status is 200': (r) => r.status === 200 });
+  const url = 'http://localhost:8000/cpu-burn?duration=0.03';
+  const res = http.get(url);
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
   sleep(0.05);
 }
